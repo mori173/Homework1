4ab63d9bb3bece379955c50ff0626335046d3858
mori173 <1076232991@qq.com>
2023-04-10 17:29:45 +0800

    第三个测试通过，commit

--- app/src/main/java/cn/edu/jnu/x2020101367/Game.java
+++ app/src/main/java/cn/edu/jnu/x2020101367/Game.java
@@ -1,12 +1,27 @@
 package cn.edu.jnu.x2020101367;
 
 public class Game {
-    private int score = 0; //初始化分数为0
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
+
     public void roll(int pins){
-        score +=pins;
+        rolls[currentRoll++] = pins;
     }
 
-    public int score() {
+    public int score() { //PPT中的类
+        int score = 0;
+        int i = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (rolls[i] + rolls[i + 1] == 10) // spare
+            {
+                score += 10 + rolls[i + 2];
+                i += 2;
+            } else {
+                score += rolls[i] + rolls[i + 1];
+                i += 2;
+            }
+        }
         return score;
+
     }
 }

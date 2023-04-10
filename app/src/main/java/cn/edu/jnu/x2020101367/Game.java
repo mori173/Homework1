859196179ab1d7e4764475c12106a6526cd6fbec
mori173 <1076232991@qq.com>
2023-04-10 17:18:32 +0800

    第二个测试通过，commit

--- app/src/main/java/cn/edu/jnu/x2020101367/Game.java
+++ app/src/main/java/cn/edu/jnu/x2020101367/Game.java
@@ -1,11 +1,12 @@
 package cn.edu.jnu.x2020101367;
 
 public class Game {
+    private int score = 0; //初始化分数为0
     public void roll(int pins){
-
+        score +=pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
--- app/src/test/java/cn/edu/jnu/x2020101367/BowlingGameUnitTest.java
+++ app/src/test/java/cn/edu/jnu/x2020101367/BowlingGameUnitTest.java
@@ -1,5 +1,6 @@
 package cn.edu.jnu.x2020101367;
 
+import org.junit.Before;
 import org.junit.Test;
 import junit.framework.TestCase;
 import static org.junit.Assert.*;
@@ -10,20 +11,25 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class BowlingGameUnitTest {
+    private Game game1;
+
+    @Before
+    public void setUp() throws Exception {
+        game1 = new Game();
+    }
+
     @Test
     //测试roll方法，投掷一次，计分不变
     public void testRollOnce() {
-        Game game = new Game();
-        game.roll(0);
-        assertEquals(0, game.score());
+        game1.roll(0);
+        assertEquals(0, game1.score());
     }
 
     @Test
     //测试roll方法，投掷两次，计分相加
     public void testRollTwice() {
-        Game game = new Game();
-        game.roll(4);
-        game.roll(5);
-        assertEquals(9, game.score());
+        game1.roll(4);
+        game1.roll(5);
+        assertEquals(9, game1.score());
     }
 }
